1aba93a09812ab8cf3e8d04f285cd3a30e7d1613
YoussifMans <youssif.ahmed.mansour.0615@gmail.com>
2025-02-25 01:28:25 +0200

    added directory-related commands (ls and cd)

--- functions.py
+++ functions.py
@@ -5,13 +5,13 @@ HOST_NAME = os.uname().nodename
 USERNAME = os.environ.get('USER') or os.environ.get('USERNAME')
 
 def tokenize(string):
-    return string.split()
+    return string.split(" ")
 
 def parse(tokens):
     command = '' if len(tokens) == 0 else tokens[0]
     
     if len(tokens) > 0:
-        tokens.pop()
+        tokens.pop(0)
     
     args = tokens
 
@@ -28,11 +28,20 @@ def parse(tokens):
         case 'whoami':
             print(USERNAME)
 
-        case 'hostname':
-            print(HOST_NAME)
+        case 'cd':
+            try:
+                os.chdir(args[0])
+            except FileNotFoundError:
+                print(f"{args[0]}: file not found!")
 
         case 'ls':
-            print()
+            output = os.listdir('.')
+
+            for i in output:
+                print(f"    {i}")
+
+        case 'hostname':
+            print(HOST_NAME)
 
         case _:
             print(f'"{command}": Bad command or filename')
